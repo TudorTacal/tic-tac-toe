a35e7f936f3d2e484e3bfa53ea3ce168db01af3c
TudorTacal <tudortacal@gmail.com>
2017-01-26 10:40:53 +0000

    Creates a TicTacToe class to serve as the game

---
+++ spec/ticTacToeSpec.js
@@ -0,0 +1,6 @@
+describe('TicTacToe', function (){
+  var game;
+  it('TicTacToe is defined', function (){
+    expect(TicTacToe).toBeDefined();
+  });
+});
---
+++ src/ticTacToe.js
@@ -0,0 +1,1 @@
+var TicTacToe = function () {};
