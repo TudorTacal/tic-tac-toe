eaa112acdde26e0410676c0165b318c139e04e56
TudorTacal <tudortacal@gmail.com>
2017-01-27 09:56:35 +0000

    Extracted an equivalent #_checkColumnsMarks as previous

--- src/ticTacToe.js
+++ src/ticTacToe.js
@@ -24,13 +24,9 @@ TicTacToe.prototype.endGame = function () {
 
 TicTacToe.prototype.checkWinner = function () {
   var fieldsArray = this.board.fields;
-  var columns = this._getColumsFromFieldsArray();
   var diagonals = [[fieldsArray[0][0],fieldsArray[1][1],fieldsArray[2][2]],[fieldsArray[0][2],fieldsArray[1][1],[fieldsArray[2][0]]]];
   if (this._checkRowMarks()) return this._declareWinner();
-  for(var j = 0; j < columns.length; j++) {
-    var column = columns[j];
-    if(column.allValuesSame()) return('You won');
-  }
+  if (this._checkColumnMarks()) return this._declareWinner();
   for(var k = 0; k < diagonals.length; k++) {
     var diagonal = diagonals[k];
     if(diagonal.allValuesSame()) return('You won');
@@ -58,12 +54,20 @@ TicTacToe.prototype._checkRowMarks = function () {
   var fieldsArray = this.board.fields;
   for(var i = 0; i < fieldsArray.length; i++){
     var row = fieldsArray[i];
-    if(row.allValuesSame()) return true
+    if(row.allValuesSame()) return true;
+  }
+};
+
+TicTacToe.prototype._checkColumnMarks = function () {
+  var columns = this._getColumsFromFieldsArray();
+  for(var i = 0; i < columns.length; i++) {
+    var column = columns[i];
+    if(column.allValuesSame()) return true;
   }
 };
 
 TicTacToe.prototype._declareWinner = function () {
-  return 'You won'
+  return 'You won';
 };
 
 Array.prototype.allValuesSame = function () {
